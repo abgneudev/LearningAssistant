ba78e461f0c4486b511adc8d2bad48c5b7321c45
Abhinav Gupta <northeastern.edu>
2024-12-13 03:37:21 -0500

    updated planner page to load new url

--- streamlit/ui/planner.py
+++ streamlit/ui/planner.py
@@ -4,6 +4,8 @@ import json
 import os
 from dotenv import load_dotenv
 
+load_dotenv()
+
 # Get the FastAPI URL from environment variables
 DEPLOYED_URL = os.getenv("DEPLOYED_URL", "http://127.0.0.1:8000")
 
